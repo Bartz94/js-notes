7ce74dedcb6ea82d5a5776c4f24ab8bde1cc1689
Bartosz Czyzewski <bart94946@gmail.com>
2024-10-08 18:10:37 +0200

    f

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import { showAddNoteModal, hideNoteModal, editNote } from './modal.js';
 import { addNote, confirmDelete } from './notes.js';
 
 const addNoteBtn = document.getElementById('addNoteBtn');
+const addNoteBtnEmpty = document.querySelector('.empty-note-content');
 const searchInput = document.getElementById('search');
 const saveNoteBtn = document.getElementById('saveNoteBtn');
 const cancelBtn = document.getElementById('cancelBtn');
@@ -11,6 +12,7 @@ const cancelDeleteBtn = document.getElementById('cancelDelete');
 
 searchInput.addEventListener('input', renderNotes);
 addNoteBtn.addEventListener('click', showAddNoteModal);
+addNoteBtnEmpty.addEventListener('click', showAddNoteModal);
 saveNoteBtn.addEventListener('click', addNote);
 cancelBtn.addEventListener('click', hideNoteModal);
 confirmDeleteBtn.addEventListener('click', confirmDelete);
